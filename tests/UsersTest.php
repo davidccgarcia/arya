6a18cada02a0edafd17c21212dc5d4d0b883886b
David García <ccristhiangarcia@gmail.com>
2017-12-14 10:06:01 -0500

    Create test the list of the users

---
+++ tests/UsersTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\User;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class UsersTest extends TestCase
+{
+    /**
+     * test the list of users.
+     *
+     * @return void
+     */
+    public function testUsersList()
+    {
+        User::create([
+            'name' => 'David García', 
+            'email' => 'ccristhiangarcia@gmail.com',
+            'password' => bcrypt('123456')
+        ]);
+
+        User::create([
+            'name' => 'Natalia Mutis', 
+            'email' => 'nataliamutis@gmail.com',
+            'password' => bcrypt('123456')
+        ]);
+
+        $this->visit('/users')
+            ->see('David García')
+            ->see('Natalia Mutis');
+    }
+}
