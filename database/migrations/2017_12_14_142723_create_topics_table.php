5345a825b0d2599279be4c136b70dcc332672c99
David García <ccristhiangarcia@gmail.com>
2017-12-14 09:40:09 -0500

    Migrations

---
+++ database/migrations/2017_12_14_142723_create_topics_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTopicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('topics', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('name', 60);
+            $table->text('description');
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+                
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('topics');
+    }
+}
