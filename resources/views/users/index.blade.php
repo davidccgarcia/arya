d839257f2762141bedb8e38d7498232a780e0b1b
David García <ccristhiangarcia@gmail.com>
2017-12-15 11:25:39 -0500

    Integrating bootstrap

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,14 +1,34 @@
-<table>
-    <thead>
-        <th>ID</th>
-        <th>name</th>
-        <th>email</th>
-    </thead>
-@foreach ($users as $user)
-    <tr>
-        <th>{{ $user->id }}</th>
-        <td>{{ $user->name }}</td>
-        <td>{{ $user->email }}</td>
-    </tr>
-@endforeach
-</table>
+@extends('layouts.app')
+
+@section('content')
+<h1 class="page-header">
+    Dashboard
+</h1>
+@if (count($users) > 0)
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h3 class="panel-title">Users</h3>
+        </div>
+        <div class="panel-body">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        @foreach ($users as $user)
+                            <th scope="row">{{ $user->id }}</th>
+                            <td>{{ $user->name }}</td>
+                            <td>{{ $user->email }}</td>
+                        @endforeach
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endif
+@endsection
